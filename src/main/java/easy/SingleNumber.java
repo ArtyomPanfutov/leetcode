3b3b490a1416eb506c22e57612b8f54e19f00e48
Artyom Panfutov <artyom.cox@gmail.com>
2021-01-24 12:42:07 +0300

    bit manipulation approach

---
+++ src/main/java/easy/SingleNumber.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 136. Single Number
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ *
+ * Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int digit : nums) {
+            if (map.containsKey(digit)) {
+                map.remove(digit);
+            } else {
+                map.put(digit, 1);
+            }
+        }
+
+        return map.keySet().iterator().next();
+    }
+
+    /**
+     * And there is more efficient solution with bit manipulation
+     */
+    public int singleNumberBitManipulationApproach(int[] nums) {
+        int a = 0;
+        for (int i : nums) {
+            a ^= i;
+        }
+        return a;
+    }
+
+}
