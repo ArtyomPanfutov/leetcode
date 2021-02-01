13eba2b7306732765553767348aa4c750d0292a9
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-01 21:55:17 +0300

    update reverse bits solution

--- src/main/java/easy/ReverseBits.java
+++ src/main/java/easy/ReverseBits.java
@@ -15,21 +15,15 @@ package easy;
 public class ReverseBits {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        for (int i = 0; i < 16; i++) {
-            n = swap(n, i, 32 - i - 1);
+        int retVal = 0;
+        int power = 31;
+
+        while (n != 0) {
+            retVal += (n & 1) << power;
+            n = n >> 1;
+            power--;
         }
-
-        return n;
-    }
-
-    private int swap(int n, int i, int j) {
-        int a = (n >> i) & 1;
-        int b = (n >> j) & 1;
-
-        if ((a ^ b) != 0) {
-            return n ^= (1 << i) | (1 << j);
-        }
-
-        return n;
+        return retVal;
     }
 }
+
