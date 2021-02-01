d462dfe952ee891cf0642d935d747c8e362b1ce0
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-01 21:13:09 +0300

    NumberOfOneBits another approach

--- src/main/java/easy/NumberOfOneBits.java
+++ src/main/java/easy/NumberOfOneBits.java
@@ -24,4 +24,16 @@ public class NumberOfOneBits {
         }
         return count;
     }
+
+    /** Bit manipulation trick */
+    public int hammingWeight(int n) {
+        int sum = 0;
+
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+
+        return sum;
+    }
 }
