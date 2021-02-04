3c6d176caf82c9742f82ffecbd83646dd947e9ff
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-04 22:32:49 +0300

    ReverseLinkedList

---
+++ src/main/java/easy/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+package easy;
+
+import datastructures.ListNode;
+
+/**
+ * 206. Reverse Linked List
+ *
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ */
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode temp = current.next;
+
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+
+        return prev;
+    }
+}
