27c9e9498585521f7488e20f0c61e5ed8bf878e3
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-14 17:41:00 +0300

    rotate array using reverse

--- src/main/java/easy/RotateArray.java
+++ src/main/java/easy/RotateArray.java
@@ -12,15 +12,46 @@ package easy;
  *
  */
 public class RotateArray {
+    /**
+     * This approach uses additional memory
+     * Time and space complexity are linear
+     *
+     * @param nums
+     * @param k
+     */
+//    public void rotate(int[] nums, int k) {
+//        int[] arr = new int[nums.length];
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            arr[(i + k) % nums.length] = nums[i];
+//        }
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            nums[i] = arr[i];
+//        }
+//    }
+
+
+    /**
+     * Another approach using reversing
+     * We can eliminate extra array therefore space complexity will be constant
+     */
     public void rotate(int[] nums, int k) {
-        int[] arr = new int[nums.length];
+        k %= nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
-            arr[(i + k) % nums.length] = nums[i];
-        }
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = arr[i];
+    private void reverse(int[] nums, int from, int to) {
+        while (from < to) {
+            int temp = nums[from];
+            nums[from] = nums[to];
+            nums[to] = temp;
+
+            from++;
+            to--;
         }
     }
 }
