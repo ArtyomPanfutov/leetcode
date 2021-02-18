2b5d38dcd107138ed78c5cdf6d2f2ab82b107129
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-18 20:54:11 +0300

    rotate image

---
+++ src/main/java/medium/RotateImage.java
@@ -0,0 +1,30 @@
+package medium;
+
+/**
+ * 48. Rotate Image
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ *
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ */
+public class RotateImage {
+
+    /**
+     * Let MM be the number of cells in the matrix.
+     *
+     * Time complexity : \mathcal{O}(M)O(M), as each cell is getting read once and written once.
+     *
+     * Space complexity : \mathcal{O}(1)O(1) because we do not use any other additional data structures.
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
