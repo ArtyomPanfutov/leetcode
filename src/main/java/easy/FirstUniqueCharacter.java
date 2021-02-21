b1a99754de065950ccacd9e2685392ca67ac7bf3
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-21 12:46:58 +0300

    first unique character

---
+++ src/main/java/easy/FirstUniqueCharacter.java
@@ -0,0 +1,35 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 387. First Unique Character in a String
+ * Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+ *
+ * Examples:
+ *
+ * s = "leetcode"
+ * return 0.
+ *
+ * s = "loveleetcode"
+ * return 2.
+ *
+ * Note: You may assume the string contains only lowercase English letters.
+ */
+public class FirstUniqueCharacter {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) return i;
+        }
+
+        return -1;
+    }
+}
