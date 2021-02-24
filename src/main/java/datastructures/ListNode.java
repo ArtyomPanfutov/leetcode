2f00f05e2c75be59d78860b3d326cf7d578a463d
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-24 23:45:30 +0300

    RemoveNthNodeFromEndofList

--- src/main/java/datastructures/ListNode.java
+++ src/main/java/datastructures/ListNode.java
@@ -12,4 +12,6 @@ public class ListNode {
         next = null;
     }
 
+    public ListNode() {}
+
 }
---
+++ src/main/java/medium/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,49 @@
+package medium;
+
+import datastructures.ListNode;
+
+/**
+ * 19. Remove Nth Node From End of List
+ *
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ *
+ * Follow up: Could you do this in one pass?
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * Example 2:
+ *
+ * Input: head = [1], n = 1
+ * Output: []
+ * Example 3:
+ *
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ */
+public class RemoveNthNodeFromEndofList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+
+        ListNode current = dummy;
+        ListNode delayed = dummy;
+
+        for (int i = 1; i <= n + 1; i++) {
+            current = current.next;
+        }
+
+        while (current != null) {
+            current = current.next;
+            delayed = delayed.next;
+        }
+
+        delayed.next = delayed.next.next;
+
+        return dummy.next;
+    }
+}
