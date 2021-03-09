a44976e4f99d0e3365f9f955de694f02ed02c127
Artyom Panfutov <artyom.cox@gmail.com>
2021-03-09 20:57:30 +0300

    power of three loop

--- src/main/java/easy/PowerOfThree.java
+++ src/main/java/easy/PowerOfThree.java
@@ -45,6 +45,18 @@ public class PowerOfThree {
         }
 
         return false;
+    }
 
+    /**
+     * Loop, but still on optimal
+     */
+    public boolean isPowerOfThreeLoop(int n) {
+        if (n < 1) return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
     }
 }
