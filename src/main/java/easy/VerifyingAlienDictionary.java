6df3cd8a78419dc8dc652e2cbbbf1f40c30812ba
Artyom Panfutov <artyom.cox@gmail.com>
2021-03-23 18:33:07 +0300

    verifying an alien dictionary

---
+++ src/main/java/easy/VerifyingAlienDictionary.java
@@ -0,0 +1,74 @@
+package easy;
+
+/**
+ * 953. Verifying an Alien Dictionary
+ *
+ * In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+ *
+ * Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+ * Output: true
+ * Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+ * Example 2:
+ *
+ * Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+ * Output: false
+ * Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+ * Example 3:
+ *
+ * Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+ * Output: false
+ * Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 20
+ * order.length == 26
+ * All characters in words[i] and order are English lowercase letters.
+ */
+public class VerifyingAlienDictionary {
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] arr = new int[order.length()];
+
+        for (int i = 0; i < order.length(); i++) {
+            arr[order.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            String s1 = words[i];
+            String s2 = words[i + 1];
+
+            if (!check(s1, s2, arr)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean check(String s1, String s2, int[] arr) {
+        int size = Math.min(s1.length(), s2.length());
+
+        for (int i = 0; i < size; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (arr[s1.charAt(i) - 'a'] > arr[s2.charAt(i) - 'a']) {
+                    return false;
+                }
+                return true;
+            }
+
+
+        }
+
+        if (s1.length() > s2.length())
+            return false;
+
+        return true;
+    }
+}
