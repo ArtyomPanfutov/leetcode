4e67647e5bdbf8d1d373c0e503565b1d5e00348d
Artyom Panfutov <artyom.cox@gmail.com>
2021-04-03 23:50:08 +0300

    gdc of strings

---
+++ src/main/java/easy/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,55 @@
+package easy;
+
+
+/**
+ * 1071. Greatest Common Divisor of Strings
+ *
+ * For two strings s and t, we say "t divides s" if and only if s = t + ... + t  (t concatenated with itself 1 or more times)
+ *
+ * Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+ *
+ *
+ * Example 1:
+ *
+ * Input: str1 = "ABCABC", str2 = "ABC"
+ * Output: "ABC"
+ * Example 2:
+ *
+ * Input: str1 = "ABABAB", str2 = "ABAB"
+ * Output: "AB"
+ * Example 3:
+ *
+ * Input: str1 = "LEET", str2 = "CODE"
+ * Output: ""
+ * Example 4:
+ *
+ * Input: str1 = "ABCDEF", str2 = "ABC"
+ * Output: ""
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= str1.length <= 1000
+ * 1 <= str2.length <= 1000
+ * str1 and str2 consist of English uppercase letters.
+ *
+ */
+public class GreatestCommonDivisorOfStrings {
+    /**
+     * Time complexity
+     * Space complexity O(n) - call stack
+     */
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+
+        int divisor = gcd(str1.length(), str2.length());
+
+        return str1.substring(0, divisor);
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+
+        return gcd(b, a % b);
+    }
+}
