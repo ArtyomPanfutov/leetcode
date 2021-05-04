4ff8cb5afebac3946d77a4bef0f7fe6364281480
Artyom Panfutov <artyom.cox@gmail.com>
2021-05-04 12:26:39 +0300

    word search replace set with boolean array

--- src/main/java/medium/WordSearch.java
+++ src/main/java/medium/WordSearch.java
@@ -1,8 +1,5 @@
 package medium;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 79. Word Search
  *Given an m x n grid of characters board and a string word, return true if word exists in the grid.
@@ -41,11 +38,12 @@ import java.util.Set;
  */
 public class WordSearch {
 
-    /** Not optimal solution */
     public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (check(board, word, 0, i, j, new HashSet<>())) {
+                if (check(board, word, 0, i, j, visited)) {
                     return true;
                 }
             }
@@ -54,47 +52,43 @@ public class WordSearch {
         return false;
     }
 
-    private boolean check(char[][] board, String word, int wordIndex, int x, int y, Set<Integer> visited) {
+    private boolean check(char[][] board, String word, int wordIndex, int x, int y, boolean[][] visited) {
         if (word.charAt(wordIndex) == board[x][y]) {
 
             int rowLength = board[0].length;
-            visited.add(convert(x, y, rowLength));
+            visited[x][y] = true;
 
             if (wordIndex == word.length() - 1) {
                 return true;
             }
 
             wordIndex++;
-            if (x - 1 >= 0 && !visited.contains(convert(x - 1, y, rowLength))) {
-                if (check(board, word, wordIndex, x - 1, y, new HashSet<Integer>(visited))) {
+            if (x - 1 >= 0 && !visited[x - 1][y]) {
+                if (check(board, word, wordIndex, x - 1, y, visited)) {
                     return true;
                 }
             }
 
-            if (x + 1 < board.length &&!visited.contains(convert(x + 1, y, rowLength))) {
-                if (check(board, word, wordIndex, x + 1, y, new HashSet<Integer>(visited))) {
+            if (x + 1 < board.length && !visited[x + 1][y]) {
+                if (check(board, word, wordIndex, x + 1, y,visited)) {
                     return true;
                 }
             }
 
-            if (y - 1 >= 0 && !visited.contains(convert(x, y - 1, rowLength))) {
-                if (check(board, word, wordIndex, x, y - 1, new HashSet<Integer>(visited))) {
+            if (y - 1 >= 0 && !visited[x][y - 1]) {
+                if (check(board, word, wordIndex, x, y - 1, visited)) {
                     return true;
                 }
             }
 
-            if (y + 1 < board[0].length && !visited.contains(convert(x, y + 1, rowLength))) {
-                if (check(board, word, wordIndex, x, y + 1, new HashSet<Integer>(visited))) {
+            if (y + 1 < board[0].length && !visited[x][y + 1]) {
+                if (check(board, word, wordIndex, x, y + 1, visited)) {
                     return true;
                 }
             }
+            visited[x][y] = false;
         }
 
         return false;
     }
-
-    private int convert(int row, int col, int rowLength) {
-        return (row * rowLength) + col;
-    }
-
 }
