7428c182e77185f46f9298e78b58e4bd246c397f
Artyom Panfutov <artyom.cox@gmail.com>
2021-06-03 21:06:24 +0300

    majority element sorting solution

--- src/main/java/easy/MajorityElement.java
+++ src/main/java/easy/MajorityElement.java
@@ -1,5 +1,7 @@
 package easy;
 
+import java.util.Arrays;
+
 /**
  * 169. Majority Element
  *
@@ -9,18 +11,36 @@ package easy;
  *
  */
 public class MajorityElement {
-    public int majorityElement(int[] nums) {
-        int count = 0;
-        Integer candidate = null;
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     */
+    public static final class BoyerMooreVoting {
+        public int majorityElement(int[] nums) {
+            int count = 0;
+            Integer candidate = null;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
+            for (int num : nums) {
+                if (count == 0) {
+                    candidate = num;
+                }
+
+                count += (candidate == num) ? 1 : -1;
             }
 
-            count += (candidate == num) ? 1 : -1;
+            return candidate;
         }
+    }
 
-        return candidate;
+    /**
+     * TIme complexity: O(nlog(n))
+     * Space complexity: O(1)
+     */
+    public static final class Sorting {
+        public int majorityElement(int[] nums) {
+            Arrays.sort(nums);
+
+            return nums[nums.length / 2];
+        }
     }
 }
