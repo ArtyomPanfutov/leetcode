efa43e41d5b798d2efbdb41d35156bbb70c6eae2
Artyom Panfutov <artyom.cox@gmail.com>
2021-06-03 20:59:34 +0300

    three sum refactor

--- src/main/java/medium/ThreeSum.java
+++ src/main/java/medium/ThreeSum.java
@@ -32,24 +32,25 @@ import java.util.*;
  */
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> arr = new ArrayList<>();
+
+        if (nums.length == 0) {
+            return arr;
+        }
+
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) break; // No reason to continue because we can't get the target anyway
-
-            if (i > 0 && nums[i - 1] == nums[i]) continue;
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // skip duplicates
+            }
 
             int j = i + 1;
             int k = nums.length - 1;
-            while (j < k) {
-                if (nums[j] + nums[k] + nums[i] == 0) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
 
-                    result.add(list);
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    arr.add(Arrays.asList(nums[i], nums[k], nums[j]));
 
                     j++;
                     k--;
@@ -63,6 +64,6 @@ public class ThreeSum {
                 }
             }
         }
-        return result;
+        return arr;
     }
 }
