dc1c2c78d082de6af9de8d5178b0f9f2139949d8
Artyom Panfutov <artyom.cox@gmail.com>
2021-06-05 21:15:37 +0300

    letter combintaions of phone number

---
+++ src/main/java/medium/LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,71 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 17. Letter Combinations of a Phone Number
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ *
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ *
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: digits = "23"
+ * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * Example 2:
+ *
+ * Input: digits = ""
+ * Output: []
+ * Example 3:
+ *
+ * Input: digits = "2"
+ * Output: ["a","b","c"]
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9'].
+ */
+public class LetterCombinationsOfPhoneNumber {
+    private static final Map<Integer, char[]> map = new HashMap<>();
+    static {
+        map.put(2, new char[] {'a', 'b', 'c'});
+        map.put(3, new char[] {'d', 'e', 'f'});
+        map.put(4, new char[] {'g', 'h', 'i'});
+        map.put(5, new char[] {'j', 'k', 'l'});
+        map.put(6, new char[] {'m', 'n', 'o'});
+        map.put(7, new char[] {'p', 'q', 'r', 's'});
+        map.put(8, new char[] {'t', 'u', 'v'});
+        map.put(9, new char[] {'w', 'x', 'y', 'z'});
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        fill(result, "", digits, 0);
+
+        return result;
+    }
+
+    private void fill(List<String> result, String current, String digits, int index) {
+        if (index == digits.length()) {
+            result.add(current);
+            return;
+        }
+
+        char[] letters = map.get(digits.charAt(index) - '0');
+
+        for (int i = 0; i < letters.length; i++) {
+            fill(result, current + letters[i], digits, index + 1);
+        }
+    }
+}
