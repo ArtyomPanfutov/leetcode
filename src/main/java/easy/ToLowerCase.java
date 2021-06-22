70a3108446fd9782e3b65f84b7e2d3099757cf7d
Artyom Panfutov <artyom.cox@gmail.com>
2021-06-22 19:16:43 +0300

    to lower case

---
+++ src/main/java/easy/ToLowerCase.java
@@ -0,0 +1,41 @@
+package easy;
+
+/**
+ * 709. To Lower Case
+ * Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "Hello"
+ * Output: "hello"
+ * Example 2:
+ *
+ * Input: s = "here"
+ * Output: "here"
+ * Example 3:
+ *
+ * Input: s = "LOVELY"
+ * Output: "lovely"
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 100
+ * s consists of printable ASCII characters.
+ */
+public class ToLowerCase {
+    public String toLowerCase(String s) {
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] += 32;
+            }
+
+        }
+
+        return new String(chars);
+    }
+}
