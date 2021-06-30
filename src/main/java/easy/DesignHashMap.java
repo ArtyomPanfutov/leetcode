8cf1e038909b04ebb86d82a37e9225e3d46f2410
Artyom Panfutov <artyom.cox@gmail.com>
2021-06-30 21:51:21 +0300

    design hash map

---
+++ src/main/java/easy/DesignHashMap.java
@@ -0,0 +1,83 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * 706. Design HashMap
+ * Design a HashMap without using any built-in hash table libraries.
+ *
+ * Implement the MyHashMap class:
+ *
+ * MyHashMap() initializes the object with an empty map.
+ * void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+ * int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+ * void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+ *
+ *
+ * Example 1:
+ *
+ * Input
+ * ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+ * [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+ * Output
+ * [null, null, null, 1, -1, null, 1, null, -1]
+ *
+ * Explanation
+ * MyHashMap myHashMap = new MyHashMap();
+ * myHashMap.put(1, 1); // The map is now [[1,1]]
+ * myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+ * myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+ * myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+ * myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+ * myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+ * myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+ * myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= key, value <= 106
+ * At most 104 calls will be made to put, get, and remove.
+ */
+public class DesignHashMap {
+    private int[] data;
+
+    /** Initialize your data structure here. */
+    public DesignHashMap() {
+        data = new int[100];
+        Arrays.fill(data, -1);
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        if (key >= data.length) {
+            int size = data.length;
+            int[] expanded = new int[key * 2];
+
+            for (int i = 0; i < expanded.length; i++) {
+                if (i < size) {
+                    expanded[i] = data[i];
+                } else {
+                    expanded[i] = -1;
+                }
+            }
+
+            data = expanded;
+        }
+
+        data[key] = value;
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return key < data.length ? data[key] : -1;
+
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        if (key < data.length) {
+            data[key] = -1;
+        }
+    }
+}
