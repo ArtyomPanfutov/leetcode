3313a6145e7d6c6f9e20912645c7a2b4e934d624
Artyom Panfutov <artyom.cox@gmail.com>
2021-08-22 12:34:33 +0300

    minimum path sum

---
+++ src/main/java/medium/MinimumPathSum.java
@@ -0,0 +1,61 @@
+package medium;
+
+/**
+ * 64. Minimum Path Sum
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+ *
+ * Note: You can only move either down or right at any point in time.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * Output: 7
+ * Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+ * Example 2:
+ *
+ * Input: grid = [[1,2,3],[4,5,6]]
+ * Output: 12
+ *
+ *
+ * Constraints:
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 100
+ */
+public class MinimumPathSum {
+    /**
+     * Time complexity: O(m * n)
+     * Space complexity: O(m * n)
+     */
+    static class DpSolution {
+        public int minPathSum(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+
+            int[][] dp = new int[m][n];
+
+            dp[m - 1][n - 1] = grid[m - 1][n - 1];
+
+            for (int i = m - 2; i >= 0; i--) {
+                dp[i][n - 1] = dp[i + 1][n - 1] + grid[i][n - 1];
+            }
+
+            for (int j = n - 2; j >= 0; j--) {
+                dp[m - 1][j] = dp[m - 1][j + 1] + grid[m - 1][j];
+            }
+
+            for (int i = m - 2; i >= 0; i--) {
+                for (int j = n - 2; j >= 0; j--) {
+                    dp[i][j] = Math.min(grid[i][j] + dp[i + 1][j], grid[i][j] + dp[i][j + 1]);
+                }
+            }
+
+            return dp[0][0];
+        }
+    }
+}
