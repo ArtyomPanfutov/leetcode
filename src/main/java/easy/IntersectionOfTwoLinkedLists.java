047bd98d99e259260a73aa0d5325f074e2d58e34
Artyom Panfutov <artyom.cox@gmail.com>
2021-11-06 22:42:49 +0300

    intersection of two linked lists constant time

--- src/main/java/easy/IntersectionOfTwoLinkedLists.java
+++ src/main/java/easy/IntersectionOfTwoLinkedLists.java
@@ -10,23 +10,43 @@ import java.util.Set;
  * Write a program to find the node at which the intersection of two singly linked lists begins.
  */
 public class IntersectionOfTwoLinkedLists {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet<>();
+    static class SetSolution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            Set<ListNode> set = new HashSet<>();
 
-        ListNode current = headA;
-        while(current != null) {
-            set.add(current);
-            current = current.next;
-        }
-
-        current = headB;
-        while(current != null) {
-            if (set.contains(current)) {
-                return current;
+            ListNode current = headA;
+            while (current != null) {
+                set.add(current);
+                current = current.next;
             }
-            current = current.next;
-        }
 
-        return null;
+            current = headB;
+            while (current != null) {
+                if (set.contains(current)) {
+                    return current;
+                }
+                current = current.next;
+            }
+
+            return null;
+        }
+    }
+
+    static class ConstantTimeSolution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null) {
+                return null;
+            }
+
+            ListNode a = headA;
+            ListNode b = headB;
+
+            while (a != b) {
+                a = a != null ? a.next : headA;
+                b = b != null ? b.next : headB;
+            }
+
+            return a;
+        }
     }
 }
