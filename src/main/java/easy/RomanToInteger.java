bb5b59c41f3e1c6eff29fb13ac3aada1ae300fc0
Artyom Panfutov <artyom.cox@gmail.com>
2023-02-27 21:59:23 +0400

    Update RomanToInteger.java

--- src/main/java/easy/RomanToInteger.java
+++ src/main/java/easy/RomanToInteger.java
@@ -51,15 +51,15 @@ import java.util.Map;
  */
 public class RomanToInteger {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I',             1);
-        map.put('V',             5);
-        map.put('X',             10);
-        map.put('L',             50);
-        map.put('C',             100);
-        map.put('D',             500);
-        map.put('M',             1000);
-
+        Map<Character, Integer> map = Map.of(
+        'I', 1,
+        'V', 5,
+        'X', 10,
+        'L', 50,
+        'C', 100,
+        'D', 500,
+        'M', 1000);
+        
         int retVal = 0;
         int prev = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
