45a2cf755947dde37efec83eea46f6a18e96cc63
Artem Panfutov <artyom.cox@gmail.com>
2023-04-18 23:00:40 +0400

    combine two tables

---
+++ src/main/java/com/panfutov/leetcode/easy/CombineTwoTables.sql
@@ -0,0 +1,34 @@
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key column for this table.
+This table contains information about the ID of some persons and their first and last names.
+
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key column for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+
+*/
+# Write your MySQL query statement below
+
+select p.firstName, p.lastName, a.city, a.state
+  from Person p
+  left join Address a
+         on a.personId = p.personId
