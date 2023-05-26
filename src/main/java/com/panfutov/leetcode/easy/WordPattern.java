1840d132815f2f37ac8ff9606efff692b2a84917
Artem Panfutov <artyom.cox@gmail.com>
2023-05-26 11:38:11 +0400

    word pattern another option

--- src/main/java/com/panfutov/leetcode/easy/WordPattern.java
+++ src/main/java/com/panfutov/leetcode/easy/WordPattern.java
@@ -40,22 +40,57 @@ import java.util.Map;
  * All the words in s are separated by a single space.
  */
 public class WordPattern {
-    public boolean wordPattern(String pattern, String s) {
-        Map<Character, String> map = new HashMap<>();
+    public static class OneHashMapSolution {
+        public boolean wordPattern(String pattern, String s) {
+            Map<Character, String> map = new HashMap<>();
 
-        String[] words = s.split(" ");
+            String[] words = s.split(" ");
 
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            Character ch = pattern.charAt(i);
+            for (int i = 0; i < words.length; i++) {
+                String word = words[i];
+                Character ch = pattern.charAt(i);
 
-            if (map.containsKey(ch)) {
-                if (!map.get(ch).equals(word)) return false;
-            } else {
-                map.put(ch, word);
+                if (map.containsKey(ch)) {
+                    if (!map.get(ch).equals(word)) return false;
+                } else {
+                    map.put(ch, word);
+                }
             }
-        }
 
-        return true;
+            return true;
+        }
+    }
+
+    public static class TwoHashMapSolution {
+        public boolean wordPattern(String pattern, String s) {
+            String[] words = s.split(" ");
+
+            if (words.length != pattern.length()) {
+                return false;
+            }
+
+            Map<Character, String> wordByLetter = new HashMap<>();
+            Map<String, Character> letterByWord = new HashMap<>();
+            for (int i = 0; i < words.length; i++) {
+                Character letter = letterByWord.get(words[i]);
+                String word = wordByLetter.get(pattern.charAt(i));
+
+                if ((letter == null && word != null) || (letter != null && word == null)) {
+                    return false;
+                }
+
+                if (letter == null && word == null) {
+                    wordByLetter.put(pattern.charAt(i), words[i]);
+                    letterByWord.put(words[i], pattern.charAt(i));
+                } else {
+                    if (!words[i].equals(word) || pattern.charAt(i) != letter) {
+                        return false;
+                    }
+                }
+
+            }
+
+            return true;
+        }
     }
 }
