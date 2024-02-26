4d6c07df18ae88ec36e3fcd2aef093e3dbe25fc5
Artyom Panfutov <artyom.cox@gmail.com>
2024-02-26 14:49:55 +0300

    add alternative solution

--- src/main/java/com/panfutov/leetcode/medium/ProductOfArrayExceptItself.java
+++ src/main/java/com/panfutov/leetcode/medium/ProductOfArrayExceptItself.java
@@ -16,20 +16,45 @@ package com.panfutov.leetcode.medium;
  * Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
  */
 public class ProductOfArrayExceptItself {
-    public int[] productExceptSelf(int[] nums) {
-        int[] result = new int[nums.length];
+    public static class NoExtraArrays {
+        public int[] productExceptSelf(int[] nums) {
+            int[] result = new int[nums.length];
 
-        result[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
+            result[0] = 1;
+            for (int i = 1; i < nums.length; i++) {
+                result[i] = result[i - 1] * nums[i - 1];
+            }
+
+            int right = 1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                result[i] *= right;
+                right *= nums[i];
+            }
+
+            return result;
         }
+    }
 
-        int right = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            result[i] *= right;
-            right *= nums[i];
+    public static class ExtraArrays {
+        public int[] productExceptSelf(int[] nums) {
+            int[] pre = new int[nums.length];
+            int[] post = new int[nums.length];
+
+            pre[0] = 1;
+            for (int i = 1; i < nums.length; i++) {
+                pre[i] = nums[i - 1] * pre[i - 1];
+            }
+
+            post[post.length - 1] = 1;
+            for (int i = post.length - 2; i >= 0; i--) {
+                post[i] = nums[i + 1] * post[i + 1];
+            }
+
+            int[] result = new int[nums.length];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = pre[i] * post[i];
+            }
+            return result;
         }
-
-        return result;
     }
 }
