eff171c9ac741faab71842b681d15e19fa606535
Artyom Panfutov <artyom.cox@gmail.com>
2024-03-26 11:15:49 +0300

    find peak element

--- src/main/java/com/panfutov/leetcode/medium/FindPeakElement.java
+++ src/main/java/com/panfutov/leetcode/medium/FindPeakElement.java
@@ -46,4 +46,32 @@ public class FindPeakElement {
             return nums.length - 1;
         }
     }
+
+    public static final class BinarySearch {
+        public int findPeakElement(int[] nums) {
+            if (nums.length == 1) {
+                return 0;
+            }
+            if (nums[0] > nums[1]) {
+                return 0;
+            }
+            if (nums[nums.length - 2] < nums[nums.length - 1]) {
+                return nums.length - 1;
+            }
+
+            int left = 1;
+            int right = nums.length - 2;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                    return mid;
+                } else if (nums[mid] < nums[mid - 1]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            throw new RuntimeException("Failed");
+        }
+    }
 }
